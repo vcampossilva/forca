1ac55ad6a686f722ba4970f962be3e937fe30c5a
Vinicius <vini.javac@gmail.com>
2016-08-23 23:34:31 -0300

    creating and executing funtion

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -1,6 +1,20 @@
 (ns forca.core
   (:gen-class))
 
+(def total-de-vidas 5)
+
+(defn perdeu [] (print "Você perdeu"))
+
+(defn jogo [vidas]
+    (if (= vidas 0)
+      (perdeu)
+      (do
+        (print vidas)
+        (jogo (dec vidas))
+      )      
+    )    
+)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
